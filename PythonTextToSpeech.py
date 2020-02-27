81e5e50a671e8e58ff1de5b6c2785df5b024a35b
sanjeev kumar <sksanjeev362@gmail.com>
2020-02-27 13:56:55 +0530

    PythonSpeechText

--- PythonTextToSpeech.py
+++ PythonTextToSpeech.py
@@ -5,6 +5,7 @@ text="Hello Python Text to Speech Tutorial"
 
 language="en"
 
+
 obj=gTTS(text=text,lang=language,slow=False)
 obj.save("audio.mp3")
 
@@ -13,3 +14,5 @@ pygame.mixer.music.load(open("audio.mp3","rb"))
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy():
     print("Playing..")
+
+print("Played")
